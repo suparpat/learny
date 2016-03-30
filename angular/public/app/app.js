6455e45e7a2d997e33fbbcb0fccb7bce550f69fa
patt <reminiz@gmail.com>
2016-03-31 01:01:41 +0700

    More structuring

---
+++ angular/public/app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('learny', [
+  'ui.router',
+  'ui.bootstrap',
+  'learny.controllers',
+  'learny.filters',
+  'learny.services',
+  'learny.directives'
+])
